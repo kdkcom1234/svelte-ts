9d24f18842d02fc4576db58f86137b62ae5029f8
Daekeun Ko <kdkcom1234@gmail.com>
2023-02-08 11:27:28 +0900

    카드 덱

--- src/pages/Card.svelte
+++ src/pages/Card.svelte
@@ -9,22 +9,40 @@
   let deck: HTMLDivElement;
   let main: HTMLDivElement;
   let support: HTMLDivElement;
+  let deckItems: HTMLDivElement[] = [null, null, null, null, null];
   let cards: HTMLDivElement[] = [null, null, null, null, null];
 
   onMount(() => {
     gsap.registerPlugin(Draggable);
 
+    let origin = { x: 0, y: 0 };
     cards.forEach((card, index) => {
       Draggable.create(card, {
         type: "x,y",
         bounds: constainer,
+        onPress: function () {
+          const elem = this as Draggable;
+          const { x, y } = elem.target.getBoundingClientRect();
+          origin = { x, y };
+          console.log("origin - ", x, y);
+        },
         onRelease: function (e: PointerEvent) {
           const elem = this as Draggable;
-          console.log(elem.x, elem.y);
+
           let hit = false;
-          [main, support].concat(cards).forEach((pos) => {
+          [main, support].forEach((pos) => {
             if (elem.hitTest(pos, "50%")) {
-              elem.applyBounds(pos);
+              const stage = pos.getBoundingClientRect();
+              console.log("stage - ", stage.x, stage.y);
+              console.log("moved - ", elem.x, elem.y);
+
+              // 위치 이동
+              gsap.to(elem.target, {
+                duration: 0.5,
+                x: stage.x - origin.x + 8,
+                y: stage.y - origin.y + 8,
+              });
+
               hit = true;
             }
           });
@@ -49,8 +67,10 @@
     <div bind:this={support}>support</div>
   </div>
   <div bind:this={deck} class="deck">
-    {#each cards as card}
-      <div bind:this={card} class="card">card</div>
+    {#each deckItems as deckItem, index}
+      <div bind:this={deckItem} class="deck-item">
+        <div bind:this={cards[index]} class="card">card {index + 1}</div>
+      </div>
     {/each}
   </div>
 </div>
@@ -58,8 +78,8 @@
 <style>
   .stage {
     display: flex;
-    margin-bottom: 4px;
-    gap: 4px;
+    margin-bottom: 20px;
+    gap: 8px;
     justify-content: center;
   }
 
@@ -71,13 +91,23 @@
 
   .deck {
     display: flex;
-    gap: 4px;
+    gap: 8px;
     justify-content: center;
+    align-items: center;
+  }
+
+  .deck-item {
+    width: 150px;
+    height: 250px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .card {
-    width: 150px;
-    height: 250px;
+    width: 134px;
+    height: 234px;
     border: 1px solid red;
     display: flex;
     align-items: center;
